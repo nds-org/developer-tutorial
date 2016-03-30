0ac558c39cf7f5cbde1263caf5c408492941246f
Mike Lambert <lambert8@illinois.edu>
2016-03-30 23:50:53 +0000

    Cloud9 working, ownCloud is close

--- cowsay/index.php
+++ cowsay/index.php
@@ -4,38 +4,9 @@
 
 <h2>Cowsay</h2>
 
-<!-- <?php 
-// Allow from any origin
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
-
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-}
-
-// Generate Cowsay
-exec('/usr/games/fortune -a | /usr/games/cowsay', $output);
-echo "<pre>";
-
-foreach ($output as $value)
-    echo $value."\n";
-
-echo "</pre>";
-?>  -->
-
-
 <?php
 echo "<pre>";
-echo "`/usr/games/fortune -a | /usr/games/cowsay`";
+echo `/usr/games/fortune -a | /usr/games/cowsay`;
 echo "</pre>";
 ?>
 
